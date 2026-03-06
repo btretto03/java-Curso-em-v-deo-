f02a8bd7656085a2dc0f25c397809511b1dfa42d
Bruno A. Tretto <bruno@Bruno.localdomain>
2026-03-06 16:18:58 -0300

    fixing inputs

--- exercicios/SegundaAula/EntradaDeDados.java
+++ exercicios/SegundaAula/EntradaDeDados.java
@@ -4,7 +4,9 @@ import java.util.Scanner; //ferramenta para ler dados
 public class EntradaDeDados {
   public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in); // Cria o "leitor" e conecta ele ao teclado (System.in)
+    System.out.println("Digite o nome do aluno: ");
     String nome = entrada.nextLine(); // Pausa o programa, lê o texto digitado e guarda na variável 'nome'
+    System.out.println("Digite a nota do aluno:");
     float nota = entrada.nextFloat(); // Pausa novamente, lê o número e guarda na variável 'nota'
     System.out.printf("A nota de %s é %.2f\n", nome, nota);
     entrada.close(); //Desliga o leitor (boa prática)
