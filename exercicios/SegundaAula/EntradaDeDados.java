6152fae2abdef9123d0e559e1b276fba73c36e87
Bruno A. Tretto <bruno@Bruno.localdomain>
2026-03-06 16:21:24 -0300

    comments

--- exercicios/SegundaAula/EntradaDeDados.java
+++ exercicios/SegundaAula/EntradaDeDados.java
@@ -4,11 +4,20 @@ import java.util.Scanner; //ferramenta para ler dados
 public class EntradaDeDados {
   public static void main(String[] args) {
     Scanner entrada = new Scanner(System.in); // Cria o "leitor" e conecta ele ao teclado (System.in)
+
     System.out.println("Digite o nome do aluno: ");
     String nome = entrada.nextLine(); // Pausa o programa, lê o texto digitado e guarda na variável 'nome'
+
     System.out.println("Digite a nota do aluno:");
     float nota = entrada.nextFloat(); // Pausa novamente, lê o número e guarda na variável 'nota'
+
     System.out.printf("A nota de %s é %.2f\n", nome, nota);
+
     entrada.close(); //Desliga o leitor (boa prática)
+
+    /*
+    * Se quiser usar o teclado para ler um num inteiro:
+    * int idade = entrada.nextInt()
+    */
   }
 }
