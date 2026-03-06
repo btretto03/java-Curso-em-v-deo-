b7c23d300a4c37c4b33e9b89eb50be7c42d95d9e
Bruno A. Tretto <bruno@Bruno.localdomain>
2026-03-06 18:15:01 -0300

    math class

---
+++ exercicios/TerceiraAula/ClasseMath.java
@@ -0,0 +1,17 @@
+package exercicios.TerceiraAula;
+
+public class ClasseMath {
+    public static void main(String[] args) {
+        double pi = Math.PI; //Valor de pi
+        System.out.println(pi);
+        System.out.println(Math.pow(5, 2)); //5^2
+        System.out.println(Math.sqrt(25)); 
+
+        System.out.println(Math.abs(-10)); //módulo
+        System.out.println(Math.floor(3.9)); //funcao piso
+        System.out.println(Math.ceil(4.2)); //funcao teto
+        System.out.println(Math.round(5.6)); //arredondamento
+        System.out.println(Math.random()); //gera um numero x aleatorio 0<x<1
+    }
+    
+}
